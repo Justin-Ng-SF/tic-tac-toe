6526918c6ce2e6376f9e25afc093c164d35f8557
Justin Ng <justinng123409@yahoo.com>
2019-11-30 04:55:15 -0800

    implemented logic for leaderboard

--- spark/src/main/java/LeaderBoard.java
+++ spark/src/main/java/LeaderBoard.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+
 public class LeaderBoard {
-    //array of players
+    /*array of Documents containing player_id, wins
+    *
+    *
+    *
+    * */
+
+    public ArrayList<PlayerDto> Player = new ArrayList<PlayerDto>();
+
+    public LeaderBoard(PlayerDto leaderBoard[]){
+
+    }
+
+    public void addToLeaderBoard(){
+
+    }
+
 
 
 
