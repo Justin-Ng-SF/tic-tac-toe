3de0a1fba0432b650fbfafd404ef7e5ec44b8b70
Justin Ng <justinng123409@yahoo.com>
2019-11-30 17:30:42 -0800

    comments

--- spark/src/main/java/LeaderBoard.java
+++ spark/src/main/java/LeaderBoard.java
@@ -15,6 +15,17 @@ public class LeaderBoard {
     }
 
     public void addToLeaderBoard(Document doc){
+        //List<String> sortedList = list.stream().sorted().collect(Collectors.toList());
+/*        MongoCursor<Document> cursor = myColection.find().iterator();
+        try {
+            while (cursor.hasNext()) {
+                Document doc = cursor.next();
+                NoteDTO oneNote = Encap.DAO(doc.get("data").toString(), doc.getObjectId("_id").toHexString(), doc.getObjectId("_id").getTimestamp());
+                noteB.setNote(oneNote);
+            }
+        } finally {
+            cursor.close();
+        }*/
 
     }
 
